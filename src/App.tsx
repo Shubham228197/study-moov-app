df939d523e060e60af6cc68ad367c7fcab10be3f
ShubhamPandey <shubham228pandey@gmail.com>
2023-05-03 17:45:02 +0530

    Initil commit plus package update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// import './App.css';
 import AboutMoov from './components/AboutMoov';
 
 function App() {
--- src/components/AboutMoov.tsx
+++ src/components/AboutMoov.tsx
@@ -1,34 +1,12 @@
-import { Paper } from "@mui/material";
-import CloseIcon from '@mui/icons-material/Close';
-import "../styling/AboutMoov.css";
+import React from 'react'
 
 const AboutMoov = () => {
   return (
     <div>
-      <Paper
-        className="about-container"
-        sx={{ borderRadius: "20px" }}
-        elevation={8}
-      >
-        <h1 className="about-headline">About Us <CloseIcon/>
-        </h1>
-        <p>
-          An idea of ​​what it might take for you to get into your dream
-          university.
-          <br />
-          <br />
-          Personalized and detailed offer of our services depending on your
-          needs.
-          <br />
-          <br />
-          Understanding what financial and time commitments will be required
-          from you to get into the university of your dreams. <br />
-          <br />
-          Discuss the strength of your portfolio and application in general
-        </p>
-      </Paper>
+      About Moov
     </div>
-  );
-};
+  )
+}
 
-export default AboutMoov;
+export default AboutMoov
+
